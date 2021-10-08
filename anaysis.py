310cf5ef4d082160ed1504f496ffc9e6d91f712f
teunvandorp <teuncandorp2002@gmail.com>
2021-10-08 12:20:42 +0200

    tellen van ziektes

--- anaysis.py
+++ anaysis.py
@@ -34,5 +34,40 @@ def read():
     return file_dictionary
 
 
+def write_csv(file_dictionary):
+    with open("results.tsv", "w") as file:
+        column_list = ["ID", "CHROM", "POS", "REF", "ALT", "AF_ESP", "AF_EXAC",
+                       "AF_TGP", "ALLELEID", "CLNDN", "CLNDNINCL", "CLNDISDB",
+                       "CLNDISDBINCL", "CLNHGVS", "CLNREVSTAT", "CLNSIG",
+                       "CLNSIGCONF", "CLNSIGINCL", "CLNVC", "CLNVCSO", "CLNVI",
+                       "DBVARID", "GENEINFO", "MC", "ORIGIN", "RS", "SSR"]
+        for value in column_list:
+            file.write(f"{value}\t")
+        file.write("\n")
+        for key in file_dictionary.keys():
+            file.write(f"{key}\t")
+            for value in file_dictionary[key]:
+                file.write(f"{value}\t")
+            file.write("\n")
+
+
+def count_illness(data_dictionary):
+    illnesses = dict()
+    for value in data_dictionary.values():
+        for illness in value[8]:
+            try:
+                illnesses[illness.split("|")[0]] += 1
+            except KeyError:
+                illnesses[illness.split("|")[0]] = 1
+    illnesses = sorted(illnesses.items(), key=lambda x: x[1], reverse=True)
+    print(illnesses)
+
+
+def analysis(data_dictionary):
+    count_illness(data_dictionary)
+
+
 if __name__ == '__main__':
     fd = read()
+    # write_csv(fd)
+    analysis(fd)
