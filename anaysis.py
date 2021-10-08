63b607bb1df3340f4bfa3d68dd705e5592bbb1de
teunvandorp <teuncandorp2002@gmail.com>
2021-10-08 10:49:22 +0200

    parsen van clinvar bestand

--- anaysis.py
+++ anaysis.py
@@ -2,13 +2,25 @@ import vcf
 
 
 def read():
+    """
+    Parser for the clinvar vcf file
+    :return: A dictoinary of the vcf file with the ID as key
+    """
+
+    # list with the column names
     column_list = ["AF_ESP", "AF_EXAC", "AF_TGP", "ALLELEID", "CLNDN",
                    "CLNDNINCL", "CLNDISDB", "CLNDISDBINCL", "CLNHGVS",
                    "CLNREVSTAT", "CLNSIG", "CLNSIGCONF", "CLNSIGINCL", "CLNVC",
                    "CLNVCSO", "CLNVI", "DBVARID", "GENEINFO", "MC", "ORIGIN",
                    "RS", "SSR"]
+
+    # create dictionary
     file_dictionary = dict()
+
+    # open file
     file = vcf.Reader(open("clinvar.vcf"))
+
+    # add entries to dictionary
     for entry in file:
         file_dictionary[int(entry.ID)] = list()
         file_dictionary[int(entry.ID)].extend([entry.CHROM, entry.POS,
@@ -18,6 +30,7 @@ def read():
                 file_dictionary[int(entry.ID)].append(entry.INFO[column])
             except KeyError:
                 file_dictionary[int(entry.ID)].append("N/A")
+
     return file_dictionary
 
 
