95ebcaaf614b4a6c84f0655061911e9b595147a1
teunvandorp <teunvandorp2002@gmail.com>
2021-11-10 13:33:08 +0100

    Code gemaakt voor data voor ML model

---
+++ filter_annotated_pathogenicity.py
@@ -0,0 +1,21 @@
+# reviewed by expert panel & practice guideline in kolom 14
+
+
+import pandas as pd
+
+
+def open_tsv(location):
+    return pd.read_csv(location, sep="\t")
+
+
+def filter_data(data):
+    return data[(data.CLNREVSTAT == "reviewed by expert panel") | (data.CLNREVSTAT == "practice guideline")]
+
+
+def write_tsv(data):
+    data.to_csv("ML data.tsv", sep="\t")
+
+
+if __name__ == '__main__':
+    filter = filter_data(open_tsv("results_new.tsv"))
+    write_tsv(filter)
