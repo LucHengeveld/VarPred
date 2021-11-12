3a2676f763cc48328f48d077b4ddffdb9390d81d
MJKorte <MJ.deKorte@student.han.nl>
2021-11-12 12:34:56 +0100

    gene code toegevoegd

--- pythonScripts/filter_annotated_pathogenicity.py
+++ pythonScripts/filter_annotated_pathogenicity.py
@@ -1,4 +1,6 @@
+from matplotlib.pyplot import get_current_fig_manager
 import pandas as pd
+import os
 
 
 def open_tsv(location):
@@ -11,10 +13,25 @@ def filter_data(data):
                 (data.CLNSIG != "Uncertain significance")]
 
 
-def write_tsv(data):
-    data.to_csv("ML data.tsv", sep="\t", index=False)
+def write_tsv(data, name):
+    data.to_csv(name, sep="\t", index=False)
+
+def getGenes(data):
+    gene_list = data['GENEINFO']
+    gene_code_list = []
+    for gene in gene_list:
+        gene_code = str(gene).split(':')[0]
+        gene_code_list.append(gene_code)
+    print(len(gene_code_list))
+    print(len(set(gene_code_list)))
+    data['GENECODE'] = gene_code_list
+    return data
+
 
 
 if __name__ == '__main__':
-    subset = filter_data(open_tsv("results_new.tsv"))
-    write_tsv(subset)
+    #subset = filter_data(open_tsv("results_new.tsv"))
+    print(os.getcwd())
+    subset = open_tsv("pythonScripts/ML data.txt")
+    subset = getGenes(subset)
+    write_tsv(subset, 'ML data fgenes.tsv')
