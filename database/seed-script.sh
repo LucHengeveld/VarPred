3594cdb28bfcb1d87d4772fc52be765310e71907
MJKorte <MJ.deKorte@student.han.nl>
2021-12-15 10:51:10 +0100

    code

---
+++ database/seed-script.sh
@@ -0,0 +1,7 @@
+mongo "mongodb"
+
+db.variants37.createIndex({CHROM: 1, POS: 1, REF: 1, ALT: 1}, {name: "compound_index_37"})
+
+db.variants38.createIndex({CHROM: 1, POS: 1, REF: 1, ALT: 1}, {name: "compound_index_38"})
+
+exit
