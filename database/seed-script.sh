bda59e391a413f069910462987bcac1ef11cc2d7
MJKorte <MJ.deKorte@student.han.nl>
2021-12-17 10:47:37 +0100

    yes

--- database/seed-script.sh
+++ database/seed-script.sh
@@ -1,3 +1,3 @@
 mongoimport --host mongodb --db varpred --collection variants37 --type json --file variant-37.json --jsonArray 
 mongoimport --host mongodb --db varpred --collection variants38 --type json --file variant-38.json --jsonArray
-mongoimport --host mongodb --db varpred --collection geneinfo --type json --file geneinfo.json --jsonArray
+#mongoimport --host mongodb --db varpred --collection geneinfo --type json --file geneinfo.json --jsonArray
