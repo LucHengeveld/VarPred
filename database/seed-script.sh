b7c70d340ab27c0321a00ddff2e542bf6f77a4ad
MJKorte <MJ.deKorte@student.han.nl>
2022-01-14 12:36:32 +0100

    changes

--- database/seed-script.sh
+++ database/seed-script.sh
@@ -2,3 +2,4 @@ mongoimport --host mongodb --db varpred --collection variants37 --type json --fi
 mongoimport --host mongodb --db varpred --collection variants38 --type json --file variant-38.json --jsonArray
 mongoimport --host mongodb --db varpred --collection medgen --type json --file medgen.json --jsonArray
 mongoimport --host mongodb --db varpred --collection genes --type json --file gene.json --jsonArray
+mongoimport --host mongodb --db varpred --collection so --type json --file clnvcso.json --jsonArray
